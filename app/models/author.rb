739a497b2cc8052796bf1d4fbf81273542f9b843
mostafa <ghazalymostafa07@gmail.com>
2024-04-27 10:24:32 +0300

    mkae validations on authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ApplicationRecord
+    validates :name,presence:true
+    validates :DOB,presence:true
+    validates :email,presence:true
+    validates :phone_number,presence:true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+
 end
