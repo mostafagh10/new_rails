58f8944e6152c8876c1b20af977515cfb7b864de
mostafa <ghazalymostafa07@gmail.com>
2024-04-27 09:31:55 +0300

    create table post + create migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_22_124508) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_27_063119) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "age"
